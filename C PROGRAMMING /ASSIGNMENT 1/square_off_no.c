d6d5d4681ed99ec7b6b79e147b3428bbda8e5f5c
Abhishek Shinde <abhishinde8079@gmail.com>
2026-01-22 21:41:35 +0530

    Add files via upload

---
+++ C PROGRAMMING /ASSIGNMENT 1/square_off_no.c
@@ -0,0 +1,11 @@
+//write a program sqare off no
+#include <stdio.h>
+ int main()
+ {
+ 	int num1,square;
+ 	num1=9;
+ 	square=num1*num1;
+ 	
+ 	printf("sqare off no is %d ",square );
+ 	 return 0;
+ }
