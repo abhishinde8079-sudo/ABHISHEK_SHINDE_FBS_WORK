c66f2684dbabaacf1c0244085eaefd6422a94f90
Abhishek Shinde <abhishinde8079@gmail.com>
2026-01-22 21:34:56 +0530

    Add files via upload

---
+++ C PROGRAMMING /ASSIGNMENT 1/swap_two_no.c
@@ -0,0 +1,13 @@
+//write a program swaping two no 
+#include <stdio.h>
+ int main()
+ {
+ 	int num1,num2,swap;
+ 	num1=3;
+ 	num2=5;
+ 	swap=num1;
+ 	num1=num2;
+ 	num2=swap;
+ 	printf("num1=3 and num2=5 after swaping num1=%d and num2=%d",num1,num2);
+ 	 return 0;
+ }
