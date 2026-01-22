e8a821f6abfe42ee608a0918e188c8388514d4fd
Abhishek Shinde <abhishinde8079@gmail.com>
2026-01-22 21:51:20 +0530

    Add files via upload

---
+++ C PROGRAMMING /ASSIGNMENT 1/area_off_triangle.c
@@ -0,0 +1,11 @@
+//write a program area off triangle 
+#include <stdio.h>
+ int main()
+ {
+ 	float base,hieght,area;
+ 	base=3;
+ 	hieght=5;
+ 	area=0.5*base*hieght;
+ 	printf("area off triangle is %f \n",area);
+ 	 return 0;
+ }
