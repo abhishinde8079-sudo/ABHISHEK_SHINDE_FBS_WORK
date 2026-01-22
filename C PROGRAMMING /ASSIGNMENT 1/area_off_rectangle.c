ed7772a304da7c908eddbfab19ac184d3bce2204
Abhishek Shinde <abhishinde8079@gmail.com>
2026-01-22 21:50:37 +0530

    Add files via upload

---
+++ C PROGRAMMING /ASSIGNMENT 1/area_off_rectangle.c
@@ -0,0 +1,11 @@
+//write a program area off rectangle  
+#include <stdio.h>
+ int main()
+ {
+ 	float len,bre,area;
+ 	len=3;
+ 	bre=5;
+ 	area=len*bre;
+ 	printf("area off rectangle is %f \n",area);
+ 	 return 0;
+ }
