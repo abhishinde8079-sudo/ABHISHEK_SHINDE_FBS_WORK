60b86e8b7b66524bc8ed237663fea876618e9075
Abhishek Shinde <abhishinde8079@gmail.com>
2026-01-22 21:33:05 +0530

    Add files via upload

---
+++ C PROGRAMMING /ASSIGNMENT 1/arae_off_cricle.c
@@ -0,0 +1,11 @@
+//write a program area off circle 
+#include <stdio.h>
+ int main()
+ {
+ 	float pi,redius,area;
+ 	pi=3.14;
+ 	redius=5;
+ 	area=pi*redius*redius;
+ 	printf("area off cricle is %f \n",area);
+ 	 return 0;
+ }
