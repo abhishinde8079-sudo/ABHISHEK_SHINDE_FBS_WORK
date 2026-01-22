2aa68015025a80504e7fb22a517e2adbd395c872
Abhishek Shinde <abhishinde8079@gmail.com>
2026-01-22 21:39:51 +0530

    Add files via upload

---
+++ C PROGRAMMING /ASSIGNMENT 1/average_off_5_no.c
@@ -0,0 +1,14 @@
+//write a program average of 4 no 
+#include <stdio.h>
+ int main()
+ {
+ 	float num1,num2,num3,num4,num5,average;
+ 	num1=3;
+ 	num2=5;
+ 	num3=4;
+ 	num4=6;
+ 	num4=4;
+ 	average=(num1+num2+num3+num4+num5)/5;
+ 	printf("average off 5 no %f \n",average);
+ 	 return 0;
+ }
