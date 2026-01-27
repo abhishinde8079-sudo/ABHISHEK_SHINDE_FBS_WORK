63227bf710af6f8b7c9e7c74f87e4692e8733f44
Abhishek Shinde <abhishinde8079@gmail.com>
2026-01-27 17:05:51 +0530

    Add files via upload

---
+++ C PROGRAMMING /ASSIGNMENT 2/uppercase_or_lovercase.c
@@ -0,0 +1,16 @@
+//ckeck the latter is capital or small
+#include<stdio.h>
+int main()
+{
+	char chr;
+	chr='D';
+	if(chr>='A'&& chr>='Z')
+	{
+		printf("latter is small");
+	}
+	else
+	{
+		printf("latter is capital");
+	}
+	return 0;
+}
