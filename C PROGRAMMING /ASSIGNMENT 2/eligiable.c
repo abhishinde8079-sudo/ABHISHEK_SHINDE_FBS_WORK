171a31264d79289b71c9da314a9b26e8e6eb3843
Abhishek Shinde <abhishinde8079@gmail.com>
2026-01-27 19:27:19 +0530

    Add files via upload

---
+++ C PROGRAMMING /ASSIGNMENT 2/eligiable.c
@@ -0,0 +1,16 @@
+//eligible or not 
+#include<stdio.h>
+int main()
+{
+	int age=23;
+	if(age>=18)
+	{
+		printf("you are eligible");
+	}
+	else
+	{
+			printf("you are not eligible");
+	}
+	return 0;
+
+}
---
+++ C PROGRAMMING /ASSIGNMENT 2/leap_year.c
@@ -0,0 +1,15 @@
+//  a given year is a leap year.
+#include<stdio.h>
+int main()
+{
+	int year=2000;
+	if (year%4==0)
+	{
+		printf("this is leap yaer");
+	}
+	else
+	{
+	    printf("this is not leap yaer");	
+	}
+	return 0;
+}
