9ec8fe21ef354ed9b56772bb0ea3556d44bf15c0
Abhishek Shinde <abhishinde8079@gmail.com>
2026-01-27 17:06:25 +0530

    Add files via upload

---
+++ C PROGRAMMING /ASSIGNMENT 2/pos_or_neg.c
@@ -0,0 +1,15 @@
+//check the num is positive or negative 
+#include<stdio.h>
+int main()
+{
+	int no=-97;
+	if(no>0)
+	{
+		printf("no is positive");
+	}
+	else
+	{
+		printf("no is nagative");
+	}
+	return 0;
+}
