62a50804c5da621c377b05d00fff91f6fc61c3b7
Abhishek Shinde <abhishinde8079@gmail.com>
2026-01-27 15:29:05 +0530

    Add files via upload

---
+++ C PROGRAMMING /ASSIGNMENT 1/persentage_off_marks.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main()
+ {
+ 	float s1,s2,s3,s4,s5,total,percentage;
+ 	s1=55;
+ 	s2=68;
+ 	s3=76;
+ 	s4=84;
+ 	s5=58;
+ 	total=s1+s2+s3+s4+s5;
+ 	percentage=((341/500.0)*100);
+ 	printf("Total Mark off out of 500 is %f \n",total);
+ 	printf("Total_percentage is %f",percentage);
+ 	return 0;
+ }
