2267bea8e8ac8a62ecb1762fa6fee1892b496de4
Abhishek Shinde <abhishinde8079@gmail.com>
2026-01-30 11:56:16 +0530

    Add files via upload

---
+++ C PROGRAMMING /ASSIGNMENT 4/table off 5.c
@@ -0,0 +1,15 @@
+//Print table for given number.
+//Input: n = 5
+//Output: 5 10 15 20 25 30 35 40 45 50
+#include<stdio.h>
+int main()
+{
+  int no=5;
+  while(no<=50)
+  {
+  	printf("%d\n",no);
+  	no+=5;
+  }
+  return 0;
+
+}
